ad1d24decde39a715acad7d59f5754ea1fb3ddfc
Zlata <zlatatokarenko17@yandex.ru>
2022-04-11 22:03:49 +0300

    added wrong/right buttons (doesn't work)

--- My project (1)/Assets/scripts/gamescript.cs
+++ My project (1)/Assets/scripts/gamescript.cs
@@ -9,6 +9,9 @@ public class gamescript : MonoBehaviour
     public Text[] answersText;
     public Text qText;
     public Button[] answerBttns = new Button[3];
+    public Sprite[] TFIcons = new Sprite[2];
+    public Image TFIcon;
+    public Text TFText; 
     List<object> qList;
     QuestionList crntQ;
     int randQ;
@@ -25,6 +28,7 @@ public class gamescript : MonoBehaviour
             randQ = Random.Range(0, qList.Count);
             crntQ = qList[randQ] as QuestionList;
             qText.text = crntQ.question;
+            qText.gameObject.SetActive(true);
             List<string> answers = new List<string>(crntQ.answers);
 
             for (int i = 0; i < crntQ.answers.Length; i++)
@@ -56,12 +60,38 @@ public class gamescript : MonoBehaviour
         for (int i = 0; i < answerBttns.Length; i++) answerBttns[i].interactable = true;
         yield break;
     }
+    IEnumerator trueOrFalse(bool check)
+    {
+        for (int i = 0; i < answerBttns.Length; i++) answerBttns[i].interactable = false;
+        for (int i = 0; i < answerBttns.Length; i++) answerBttns[i].gameObject.GetComponent<Animator>().SetTrigger("Out");
+        qText.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        if (!TFIcon.gameObject.activeSelf) TFIcon.gameObject.SetActive(true);
+        else TFIcon.gameObject.GetComponent<Animator>().SetTrigger("In");
+        if (check)
+        {
+            TFIcon.sprite = TFIcons[0];
+            TFText.text = "Правильный ответ! :)";
+            yield return new WaitForSeconds(1.5f);
+            TFIcon.gameObject.GetComponent<Animator>().SetTrigger("Out");
+            qList.RemoveAt(randQ);
+            questionGenerate();
+            yield break;
+        }
+        else
+        {
+            TFIcon.sprite = TFIcons[1];
+            TFText.text = "Неправильный ответ! :(";
+            yield return new WaitForSeconds(1.5f);
+            TFIcon.gameObject.GetComponent<Animator>().SetTrigger("Out");
+            questionGenerate();
+            yield break;
+        }
+    }
     public void answersBttns(int index)
     {
-        if (answersText[index].text.ToString() == crntQ.answers[0]) print("Правильный ответ! :)");
-        else print("Неправильный ответ! :(");
-        qList.RemoveAt(randQ);
-        questionGenerate();
+        if (answersText[index].text.ToString() == crntQ.answers[0]) StartCoroutine(trueOrFalse(true));
+        else StartCoroutine(trueOrFalse(false));
     }
 }
 [System.Serializable]
